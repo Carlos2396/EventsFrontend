caa2db6463c09a1d02ffb1a6917f5355abd73487
Mónica Pérez Martín <monica.p.m@outlook.com>
2018-10-21 13:11:52 -0500

    Added retrieve for extra component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,14 @@ import { NotificationService } from './services/notification.service';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { NotificationTestingComponent } from './components/notification-testing/notification-testing.component';
 import { FormsModule } from '@angular/forms';
+import { RetrieveComponent } from './components/extras/retrieve/retrieve.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
-    NotificationTestingComponent
+    NotificationTestingComponent,
+    RetrieveComponent
   ],
   imports: [
     BrowserModule,
