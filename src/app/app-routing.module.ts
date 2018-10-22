0dc773135a0269c8af4dfb52e2dea8a8c5becaf1
DarkHaliax96 <angel.r.ruiz.m@gmail.com>
2018-10-22 15:03:43 -0500

    Added events to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
+import { EventsIndexComponent } from './components/events/events-index/events-index.component';
 
 const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
+    { path: 'events', component: EventsIndexComponent}
 ];
 
 @NgModule({
