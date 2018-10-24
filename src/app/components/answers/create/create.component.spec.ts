ac2487bc84606515143eda233b5cfd5bd91b32d6
Mónica Pérez Martín <monica.p.m@outlook.com>
2018-10-24 10:37:23 -0500

    Fixed answers create component testing, and now it works

--- src/app/components/answers/create/create.component.spec.ts
+++ src/app/components/answers/create/create.component.spec.ts
@@ -1,6 +1,15 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 import { CreateComponent } from './create.component';
+import { AuthService } from 'src/app/services/auth.service';
+import { NotificationService } from 'src/app/services/notification.service';
+import { APIService } from 'src/app/services/api.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Extra } from 'src/app/models/extra.model';
+import { Event } from 'src/app/models/event.model';
+import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpHandler } from '@angular/common/http';
 
 describe('CreateComponent', () => {
   let component: CreateComponent;
@@ -8,15 +17,45 @@ describe('CreateComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CreateComponent ]
+      imports: [
+        RouterTestingModule,
+        FormsModule
+      ],
+      providers: [
+        AuthService,
+        NotificationService,
+        APIService,
+        HttpClient,
+        HttpHandler
+      ],
+      declarations: [ CreateComponent
+      ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
+    TestBed.resetTestEnvironment();
+    TestBed.initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
+
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        FormsModule
+      ],
+      providers: [
+        AuthService,
+        NotificationService,
+        APIService,
+        HttpClient,
+        HttpHandler
+      ],
+      declarations: [ CreateComponent
+      ]
+    });
+
     fixture = TestBed.createComponent(CreateComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
