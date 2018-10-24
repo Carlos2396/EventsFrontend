9e4d85196fd1949ca58250f19889435e5c39d7ac
Mónica Pérez Martín <monica.p.m@outlook.com>
2018-10-24 09:09:13 -0500

    Added more modifications to the show event interface

--- src/app/components/events/events-show/events-show.component.ts
+++ src/app/components/events/events-show/events-show.component.ts
@@ -4,6 +4,7 @@ import { NotificationService } from 'src/app/services/notification.service';
 import { APIService } from 'src/app/services/api.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-events-show',
@@ -12,13 +13,16 @@ import { HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 })
 export class EventsShowComponent implements OnInit {
 
-  constructor(private notifications: NotificationService, private api:APIService, private router:Router, private route: ActivatedRoute) { }
+  constructor(private auth: AuthService,private notifications: NotificationService, private api:APIService, private router:Router, private route: ActivatedRoute) { }
 
   event: Event;
   id: Number;
+  user_id: Number;
+  available_guests: number;
 
   ngOnInit() {
     this.id = parseInt(this.route.snapshot.params.id);
+    this.user_id = this.auth.getUser().id;
     this.api.retrieve(this.api.models.EVENT, this.id)
       .subscribe(
         (res: Event)=>{
@@ -28,7 +32,7 @@ export class EventsShowComponent implements OnInit {
           this.notifications.handleError(err);
         }
       )
-      
+    this.available_guests = this.event.guest_capacity - this.event.attendees.length;
   }
 
 }
