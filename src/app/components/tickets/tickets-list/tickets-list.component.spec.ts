bcbd318cb49d8533f7b5567c5bcfe2f068088503
DarkHaliax96 <angel.r.ruiz.m@gmail.com>
2018-10-24 10:23:16 -0500

    Correction in tickets index component

--- src/app/components/tickets/tickets-list/tickets-list.component.spec.ts
+++ src/app/components/tickets/tickets-list/tickets-list.component.spec.ts
@@ -1,6 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TicketsListComponent } from './tickets-list.component';
+import { Event } from "../../../models/event.model";
+import { Location } from "../../../models/location.model";
+import { Sponsor } from "../../../models/sponsor.model";
+import { User } from "../../../models/user.model";
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('TicketsListComponent', () => {
   let component: TicketsListComponent;
@@ -8,7 +14,9 @@ describe('TicketsListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TicketsListComponent ]
+      declarations: [ TicketsListComponent ],
+      imports: [HttpClientModule, RouterTestingModule],
+      providers: [],
     })
     .compileComponents();
   }));
