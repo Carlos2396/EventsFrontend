ea6a402d1152a9cf2b6f00cb61112885fb6427ad
DarkHaliax96 <angel.r.ruiz.m@gmail.com>
2018-10-24 10:30:12 -0500

    Correction in sponsors index component

--- src/app/components/sponsors/sponsors-index/sponsors-index.component.spec.ts
+++ src/app/components/sponsors/sponsors-index/sponsors-index.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SponsorsIndexComponent } from './sponsors-index.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('SponsorsIndexComponent', () => {
   let component: SponsorsIndexComponent;
@@ -8,7 +10,8 @@ describe('SponsorsIndexComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SponsorsIndexComponent ]
+      declarations: [ SponsorsIndexComponent ],
+      imports: [HttpClientModule, RouterTestingModule]
     })
     .compileComponents();
   }));
