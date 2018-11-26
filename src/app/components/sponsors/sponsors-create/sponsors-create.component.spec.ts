ff510952291ebf637b5006290b3dc658cc8e764e
DarkHaliax96 <angel.r.ruiz.m@gmail.com>
2018-11-26 17:26:19 -0600

    Correction in sponsors 2

--- src/app/components/sponsors/sponsors-create/sponsors-create.component.spec.ts
+++ src/app/components/sponsors/sponsors-create/sponsors-create.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { SponsorsCreateComponent } from './sponsors-create.component';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule } from '@angular/forms';
 
 describe('SponsorsCreateComponent', () => {
   let component: SponsorsCreateComponent;
@@ -11,7 +12,8 @@ describe('SponsorsCreateComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ SponsorsCreateComponent ],
-      imports: [HttpClientModule, RouterTestingModule]
+      imports: [HttpClientModule, RouterTestingModule, 
+        FormsModule]
     })
     .compileComponents();
   }));
