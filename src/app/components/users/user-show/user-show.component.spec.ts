7b692b2eb53ce520fb79d24193790002b20b3875
DarkHaliax96 <angel.r.ruiz.m@gmail.com>
2018-11-26 18:03:41 -0600

    Correction in tests

--- src/app/components/users/user-show/user-show.component.spec.ts
+++ src/app/components/users/user-show/user-show.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { UserShowComponent } from './user-show.component';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule } from '@angular/forms';
 
 describe('UserShowComponent', () => {
   let component: UserShowComponent;
@@ -11,7 +12,8 @@ describe('UserShowComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ UserShowComponent ],
-      imports: [HttpClientModule, RouterTestingModule]
+      imports: [HttpClientModule, RouterTestingModule, 
+        FormsModule]
     })
     .compileComponents();
   }));
